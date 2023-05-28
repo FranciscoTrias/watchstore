aabfc78408a844293727b168d1d2c1516b58c23c
Francisco Trias <triasfran@gmail.com>
2023-05-27 22:34:08 -0300

    Navbar items align

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,21 +7,20 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <Box width="100%" paddingBottom="3.5rem">
+    <Box width="100%" >
       <Grid
         container
         position="fixed"
         justifyContent="space-evenly"
-        paddingTop="0.5rem"
-        paddingBottom="0.5rem"
+        
         paddingLeft="0.25rem"
         paddingRight="0.25rem"
-        boxShadow="2px 2px 2px grey"
         bgcolor="white"
         sx={{zIndex: 1}}
       >
-        <Grid item alignSelf="center">
-          <Box display="flex">
+        <Grid item alignSelf="center" textAlign="center" width="120px" >
+          <Box>
+            <Box textAlign="center">
             <a href="#">
               <Typography paddingRight="0.5rem" cursor="pointer" variant="h6">
                 Shop
@@ -30,15 +29,17 @@ const Navbar = () => {
             <a href="#">
               <Typography variant="h6">About</Typography>
             </a>
+
+            </Box>
           </Box>
         </Grid>
-        <Grid item alignSelf="center">
+        <Grid item alignSelf="center" textAlign="center" width="120px">
           <a href="/">
             <Typography variant="h4"> Logo </Typography>
           </a>
         </Grid>
-        <Grid item alignSelf="center">
-          <Box display="flex">
+        <Grid item alignSelf="center" textAlign="center" width="120px">
+          <Box>
             <a href="#">
               <AccountCircleOutlined transform="scale(1.2)" />
             </a>
