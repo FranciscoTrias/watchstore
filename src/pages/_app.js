3e270f00bd1b953aa8f8b39039589f5c757ada0a
Francisco Trias <triasfran@gmail.com>
2023-05-28 22:49:31 -0300

    Card implementation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,11 +5,7 @@ import Head from 'next/head';
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      <link rel="preconnect" href="https://rsms.me/"/>
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
-
-    </Head>
+    
 
       <Navbar />
       <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,53 @@
-import React from "react";
+import React from 'react';
 
-import { Button, Box, Typography, Grid} from "@mui/material";
-import Homepagebanner from "@/components/HomepageBanner";
-
-
-
+import { Button, Box, Typography, Grid } from '@mui/material';
+import Homepagebanner from '@/components/HomepageBanner';
+import ProductFrontCard from '@/shared/components/ProductFrontCard';
 
 const Home = () => {
   return (
     <>
+      <Grid container spacing={0} sx={{ flexGrow: 2 }}>
+        <Grid item xs={12}>
+          <Homepagebanner></Homepagebanner>
+        </Grid>
 
-    <Grid container spacing={0} sx={{ flexGrow: 2}}>
-  
-  <Grid item xs={12}>
-    
-    <Homepagebanner>
+        <Grid
+          item
+          xs={12}
+          sx={{ marginTop: '2.5rem', marginLeft: '5rem', marginRight: '5rem' }}
+        >
+          <Box display="flex" sx={{ height: '593px', width:'100%' }}>
+            <ProductFrontCard
+              srcImage="/assets/images/Product1.jpg"
+              productTitle="Casio G-Shock"
+              productDescription="Introducing the Casio G-Shock: A game-changer in sports watches."
+            ></ProductFrontCard>
 
-    </Homepagebanner>
-            
-  </Grid>
-  
-  
-  
-  <Grid item xs={12}>
-      <Box bgcolor='red'> 
-        box2
+            <ProductFrontCard
+              srcImage="/assets/images/Product2.jpg"
+              productTitle="Casio LTP-1310"
+              productDescription="Where sophistication meets precision, elevating your style with every tick."
+            ></ProductFrontCard>
+          </Box>
+        </Grid>
+
+        <Grid item xs={2}>
+          <Box bgcolor="cyan">
+            box3
+            <img src="/assets/images/Product1.jpg"></img>
+          </Box>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Box bgcolor="green">box3</Box>
+        </Grid>
+      </Grid>
+      <Box display="flex" justifyContent="center" alignContent="center">
+        <Typography variant="h2">Welcome to Focus</Typography>
+        <h2>Welcome to Focus</h2>
       </Box>
-  </Grid>
-
-  <Grid item xs={2}>   
-      <Box bgcolor='cyan'> 
-        box3
-      </Box>
-  </Grid>
-  
-  <Grid item xs={4}>
-      <Box bgcolor='green'> 
-        box3
-      </Box>
-  </Grid>
-
-</Grid>
-    <Box display="flex" justifyContent="center" alignContent="center">
-      <Typography variant="h2">Welcome to Focus</Typography>
-      <h2>Welcome to Focus</h2>
-    
-    </Box>
-    
     </>
-    
   );
-}
+};
 export default Home;
