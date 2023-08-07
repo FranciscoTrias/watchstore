f67ae30e5cab1a5476e81d4b6bb84d938b7ba25d
Alan del Fresno <alandelfresno2@gmail.com>
2023-08-06 21:25:22 -0300

    Remove unnecessary code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import {  Box, Grid } from '@mui/material';
 
-import { Button, Box, Typography, Grid } from '@mui/material';
 import Homepagebanner from '@/components/HomepageBanner';
 import ProductFrontCard from '@/shared/components/ProductFrontCard';
-import Image from 'next/image';
 
 const Home = () => {
   return (
@@ -39,24 +38,7 @@ const Home = () => {
             />
           </Box>
         </Grid>
-
-        <Grid item xs={2}>
-          <Box bgcolor="cyan">
-            box3
-            <Image alt="product image" src="/assets/images/Product1.jpg" />
-          </Box>
-        </Grid>
-
-        <Grid item xs={12}>
-          <Box bgcolor="green" height={{ sm: '100px' }}>
-            box3
-          </Box>
-        </Grid>
       </Grid>
-      <Box display="flex" justifyContent="center" alignContent="center">
-        <Typography variant="h2">Welcome to Focus</Typography>
-        <h2>Welcome to Focus</h2>
-      </Box>
     </>
   );
 };
